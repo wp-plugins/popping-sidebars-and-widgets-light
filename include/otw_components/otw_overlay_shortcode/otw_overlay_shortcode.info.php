b2b62a1fb0b997cd50c752a5412a0b01fda6cc90
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-05 08:49:28 +0000

    version 1.2 tag
    git-svn-id: https://plugins.svn.wordpress.org/popping-sidebars-and-widgets-light/tags/1.2@1128091 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/otw_components/otw_overlay_shortcode/otw_overlay_shortcode.info.php
@@ -0,0 +1,16 @@
+<?php
+/**
+Component Name: OTW Overlay Shortcode
+Plugin URI: http://OTWthemes.com
+Description:  OTW Overlay Shortcode
+Author: OTWthemes.com
+Version: 1.1
+Author URI: http://themeforest.net/user/OTWthemes
+*/
+
+$otw_component = array();
+$otw_component['name']       = 'OTW Overlay Shortcode';
+$otw_component['version']    = '2000.1';
+$otw_component['class_name'] = 'OTW_Overlay_Shortcode';
+
+?>
---
+++ otw_popping_sidebars_and_widgets.php
@@ -0,0 +1,65 @@
+<?php
+/**
+Plugin Name: Popping Sidebars and Widgets Light
+Plugin URI: http://OTWthemes.com
+Description:  Create custom popping layouts with sidebars and widgets in just a few clicks. 
+Author: OTWthemes.com
+Version: 1.2
+
+Author URI: http://themeforest.net/user/OTWthemes
+*/
+
+load_plugin_textdomain('otw_pswl',false,dirname(plugin_basename(__FILE__)) . '/languages/');
+
+$otw_pswl_plugin_url = plugin_dir_url( __FILE__);
+
+$otw_pswl_js_version = '1.1';
+$otw_pswl_css_version = '1.2';
+
+$otw_sbm_widget_settings = array( 
+	'before_widget' => array( __( 'HTML to be placed before every widget.', 'otw_pswl' ), '<div id="%1$s" class="widget %2$s">' ),
+	'after_widget' => array( __( 'HTML to be placed after every widget.', 'otw_pswl' ), '</div>' ),
+	'before_title' =>array( __( 'HTML to be placed before every widget title.', 'otw_pswl' ), ' <h3 class="widgettitle">' ),
+	'after_title' =>array(  __( 'HTML to be placed after every widget title.', 'otw_pswl' ), '</h3>' )
+);
+
+//include functons
+require_once( plugin_dir_path( __FILE__ ).'/include/otw_pswl_functions.php' );
+require_once( plugin_dir_path( __FILE__ ).'/include/otw_pswl_core.php' );
+
+//components
+$otw_pswl_grid_manager_component = false;
+$otw_pswl_grid_manager_object = false;
+$otw_pswl_shortcode_component = false;
+$otw_pswl_form_component = false;
+$otw_pswl_validator_component = false;
+$otw_pswl_overlay_component = false;
+$otw_pswl_overlay_object = false;
+
+//load core component functions
+@include_once( 'include/otw_components/otw_functions/otw_functions.php' );
+
+if( !function_exists( 'otw_register_component' ) ){
+	wp_die( 'Please include otw components' );
+}
+
+//register grid manager component
+otw_register_component( 'otw_overlay_grid_manager', dirname( __FILE__ ).'/include/otw_components/otw_overlay_grid_manager_light/', $otw_pswl_plugin_url.'/include/otw_components/otw_overlay_grid_manager_light/' );
+
+//register form component
+otw_register_component( 'otw_form', dirname( __FILE__ ).'/include/otw_components/otw_form/', $otw_pswl_plugin_url.'/include/otw_components/otw_form/' );
+
+//register validator component
+otw_register_component( 'otw_validator', dirname( __FILE__ ).'/include/otw_components/otw_validator/', $otw_pswl_plugin_url.'/include/otw_components/otw_validator/' );
+
+//register shortcode component
+otw_register_component( 'otw_overlay_shortcode', dirname( __FILE__ ).'/include/otw_components/otw_overlay_shortcode/', $otw_pswl_plugin_url.'/include/otw_components/otw_overlay_shortcode/' );
+
+//register overlay component
+otw_register_component( 'otw_overlay', dirname( __FILE__ ).'/include/otw_components/otw_overlay_light/', $otw_pswl_plugin_url.'/include/otw_components/otw_overlay_light/' );
+
+add_action('init', 'otw_pswl_init' );
+
+if( is_admin() ){
+	add_action( 'wp_ajax_otw_pswl_admin_settings', 'otw_pswl_admin_settings' );
+}
