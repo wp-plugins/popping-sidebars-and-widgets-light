49c5da3898910a3f1be6b8479cfc471d112affd2
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-03 13:34:39 +0000

    version change to 1.5
    git-svn-id: https://plugins.svn.wordpress.org/popping-sidebars-and-widgets-light/tags/1.5@1191819 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/otw_components/otw_overlay_grid_manager_light/otw_overlay_grid_manager.info.php
@@ -0,0 +1,16 @@
+<?php
+/**
+Component Name: OTW Overlay Grid Manager Light
+Plugin URI: http://OTWthemes.com
+Description:  OTW Grid Manager
+Author: OTWthemes.com
+Version: 0.001 light version
+Author URI: http://themeforest.net/user/OTWthemes
+*/
+
+$otw_component = array();
+$otw_component['name']       = 'OTW Overlay Grid Manager';
+$otw_component['version']    = '0.001';
+$otw_component['class_name'] = 'OTW_Overlay_Grid_Manager';
+
+?>
